2a97b929a16f722cf69af941e537dd1dea5c81bf
Francis759 <francis.dowuona@amalitech.org>
2021-06-09 07:24:36 +0000

    Modified javascript
    Modified script, Added eventListener

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
-function calculateBMI(){
-
-	let weight = document.getElementById("weightinput").value;
-	let height = document.getElementById("heightinput").value;
-	//
-	let output = document.getElementById("bmivalue-holder");
-	//
-	try {
-		if((isNaN(weight) && isNaN(height)) || (weight==="" && isNaN(height)) ||
-		(height==="" && isNaN(weight)) || (height==="" && weight==="")){
-			throw 'Weight is missing or invalid \nHeight is missing or invalid'
-		}
-		if(weight==="" || isNaN(weight)){
-			throw 'Weight is missing or invalid'
-		}
-		if(height==="" || isNaN(height)){
-			throw 'Height is missing or invalid'
-		}
-		let bmiValue = (weight/Math.pow(height,2)).toFixed(1);
-		let str = ""
-		if(bmiValue < 18.5){
-			str = "underweight";
-			output.innerHTML="Your BMI is "+ bmiValue +", so you are\xa0" + str.bold();
-		}
-		else if(bmiValue>= 18.5 && bmiValue<=24.9){
-			str = "normal weight";
-			output.innerHTML = "Your BMI is "+bmiValue +", so you have a\xa0" + str.bold();
-		}
-		else if(bmiValue>= 25 && bmiValue <=29.9){
-			str = "overweight";
-			output.innerHTML = "Your BMI is "+bmiValue +", so you are\xa0" + str.bold();
-		}
-		else if(bmiValue>= 30.0 && bmiValue <=34.9){
-			str = "obese";
-			output.innerHTML = "Your BMI is "+bmiValue +", so you are\xa0" + str.bold();
-		}
-		else{
-			str = "extremely obese";
-			output.innerHTML = "Your BMI is "+bmiValue +", so you are\xa0" + str.bold();
-		}
-		
-	} catch (error) {
-		window.alert(error);
-		output.innerHTML="";
-	}	
+function calculateBMI(weight, height){
+	return (weight/Math.pow(height,2)).toFixed(1);
 }
+
+function displayBMIResult(){
+
+	let weight  = document.getElementById("weightinput").value;
+	let height  = document.getElementById("heightinput").value;
+	let displayElement = document.getElementById("bmivalue-holder");
+
+	if(weight === "" && height === "" || isNaN(weight) && isNaN(height)
+	   || weight==="" && isNaN(height) || isNaN(weight) && height===""){
+		   window.alert("Weight is missing or invalid\nHeight is missing or invalid")
+	   }
+	   else if(isNaN(height) || height === ""){
+		window.alert("Height is missing or invalid")
+	   }
+	   else if(weight ==="" || isNaN(weight)){
+		window.alert("Height is missing or invalid")
+	   }
+	   else{
+		   let bmiValue = calculateBMI(weight,height);
+		   if(bmiValue<18.5){
+			   displayElement.innerHTML="Your BMI is "+bmiValue+", so you are\xa0<strong>underweight</strong>"
+		   }
+		   else if(bmiValue>=18.5 && bmiValue<=24.9){
+			 displayElement.innerHTML="Your BMI is "+bmiValue+", so you have a\xa0<strong>normal weight</strong>";
+		   }
+		   else if(bmiValue>=25 && bmiValue<=29.9){
+			displayElement.innerHTML="Your BMI is "+bmiValue+", so you are\xa0<strong>overweight</strong>";
+		  }
+		  else if(bmiValue>=30 && bmiValue<=34.9){
+			displayElement.innerHTML="Your BMI is "+bmiValue+", so you are\xa0<strong>obese</strong>";
+		  }
+		  else{
+			displayElement.innerHTML="Your BMI is "+bmiValue+", so you are\xa0<strong>extremely obese</strong>";
+		  }
+	   }
+}
+
+document.getElementById("calculate-btn").addEventListener("click",displayBMIResult);
